0a582e93c7c7ed510ff8841797852b7890634031
Alessandro Baio <77743955+ale-006@users.noreply.github.com>
2021-10-02 18:50:05 +0200

    Update music.js

--- music.js
+++ music.js
@@ -2,7 +2,7 @@ const Discord = require("discord.js");
 const Bot = new Discord.Client();
 const yts = require('yt-search');
 const ytdl = require('ytdl-core');
-Bot.login("token");
+Bot.login("ODkwOTc5OTIxMjU2Njc3NDU4.YU3rtg.2XdFrededbBTmuvbE0firEsWvec");
 
 var prefisso = "$";
 var CanzoneInRiproduzione;
@@ -19,26 +19,33 @@ async function Musica(message){
     if(!message.guild){
         return
     }
-    if(message.content == prefisso + "helpMusic"){
+    if(message.content == prefisso + "help"){
         var Embed = new Discord.MessageEmbed()
-        .setTitle("Ecco i comandi per la musica:")
-        .addField(prefisso + "playnow", "cancella le canzoni nella coda, e riproduce subito la canzone richiesta")
-        .addField(prefisso + "play", "cerca la canzone richiesta e ti da 10 risultati, scegli la canzone giusta facendo: " + prefisso + "choose + numero canzone")
-        .addField(prefisso + "coda", "mostra la coda di canzoni")
-        .addField(prefisso + "skip", "riproduce la canzone successiva nella coda")
-        .addField(prefisso + "isplaying", "mostra i dettagli della canzone in riproduzione")
-        .addField(prefisso + "leave", "abbandona la chat vocale")
-        .addField(prefisso + "pause", "mette in pausa la canzone")
-        .addField(prefisso + "resume", "riproduce la canzone da capo se in pause")
-        .setAuthor("bot by tyger 375#4141")
+        .setAuthor(message.author.tag, message.author.displayAvatarURL())
+        .setColor("BLUE")
+        .addField("▫️" + prefisso + "playnow", "Cancella le canzoni nella coda, e riproduce subito la canzone richiesta.")
+        .addField("▫️" + prefisso + "play", "Cerca una canzone. Ti da 10 opzioni.")
+        .addField("▫️" + prefisso + "choose", "Seleziona la canzone tra le 10 opzioni.")
+        .addField("▫️" + prefisso + "coda", "Mostra la coda di riproduzione.")
+        .addField("▫️" + prefisso + "skip", "Riproduci la canzone successiva.")
+        .addField("▫️" + prefisso + "isplaying", "Mostra dettagli sulla canzone in riproduzione.")
+        .addField("▫️" + prefisso + "leave", "Abbandona il canale vocale.")
+        .addField("▫️" + prefisso + "pause", "Metti in pausa una canzone.")
+        .addField("▫️" + prefisso + "resume", "Riproduci la canzone da zero, se in pausa.")
+        .addField("▫️" + prefisso + "credits, " + prefisso + "status, " + prefisso + "invite, " + prefisso + "support", "Quei comandi utili che non possono mancare...")
         message.channel.send(Embed)
     }
-    if(message.member.voice.channel == null){
-        return;
+    if(message.member.voice.channel == null && message.content.startsWith(prefisso) && !message.content == "$status" && !message.content == "$help" && !message.content == "$invite") {
+        let embed = new Discord.MessageEmbed()
+        .setAuthor(message.author.tag, message.author.displayAvatarURL())
+        .setDescription("**Entra in un canale vocale.**")
+        .setColor("BLUE")
+        return message.channel.send(embed)
     }
     if(message.content.startsWith(prefisso + "playnow")){
         var lunghezza = message.content.length;
         var canzone = message.content.slice(11, lunghezza);
+        if(!canzone) return message.channel.send(":x: **Specifica un titolo o un autore.**")
         const res = await yts(canzone);
         let video = res["all"][0];
         CanzoneInRiproduzione = video.url;
@@ -47,6 +54,7 @@ async function Musica(message){
         coda.push(video.url);
         var ulrVideo = video.url.toString()
         setTimeout(async function() {
+
             if(message.guild.me.voice.channel == null){
                 dispatcher.setVolume(1);
                 dispatcher.on("finish", end => {
@@ -68,13 +76,9 @@ async function Musica(message){
         }, 2000 )
     }
     if(message.content.startsWith(prefisso + "choose")){
-        if(CodaCanzoni == null){
-            message.channel.send("Non c'è nessuna canzone")
-            return;
-        }
         var QualeCanzone = message.content.split(" ")[1]
         if(QualeCanzone == null){
-            message.channel.send("Inserisci un numero valido")
+            message.channel.send(":x: **Specifica un numero valido.**\nPrima di usare `$choose`, devi usare `$play`.")
             return;
         }
         try {
@@ -89,7 +93,6 @@ async function Musica(message){
                 CanzoneInRiproduzione = video.url;
                 message.channel.send("Riproduco: " + res["all"][0]["title"]);
                 coda.push(video.url);
-                console.log(coda);
                 voiceChannel.join().then(connection =>{
                     dispatcher = connection.play(ytdl(video.url));
                     dispatcher.on("finish", end => {
@@ -114,15 +117,19 @@ async function Musica(message){
                 }).catch(err => console.log(err));
             }
         } catch (error) {
-            console.log(error)
+            message.channel.send(":x: **C'è stato un errore.**\nControlla il comando `$help`.")
         }
     }
     if(message.content.startsWith(prefisso + "play") && !(message.content.startsWith(prefisso + "playnow"))){
         CodaCanzoni.length = 0;
         var lunghezza = message.content.length;
-        var canzone = message.content.slice(8, lunghezza);
+        var canzone = message.content.slice(5, lunghezza);
+        if(!canzone) return message.channel.send(":x: **Specifica un titolo o un autore.**")
         const res = await yts(canzone);
         var Embed = new Discord.MessageEmbed()
+        .setAuthor(message.author.tag, message.author.displayAvatarURL())
+        .setColor("BLUE")
+        .setDescription("**Ecco le canzoni trovate per:** `" + canzone + " `.")
         if(res["all"].length < 10){
             for (let i = 0; i < res["all"].length; i++) {
                 const element = res["all"][i];
@@ -141,13 +148,18 @@ async function Musica(message){
     }
     if(message.content == prefisso + "coda"){
         var embed = new Discord.MessageEmbed()
-        .setTitle("CANZONI")
-        .setDescription("canzoni nella coda:")
-        await Promise.all(coda.map(async (i) => {
-            const res = await yts(i);
-            let video = res["all"][0];
-            embed.addField("canzone: ", video.title);
-        }));
+        .setAuthor(message.author.tag, message.author.displayAvatarURL())
+        .setColor("BLUE")
+        if(coda.length == 0){
+          embed.addField("Non ci sono canzoni in riproduzione.", "Scrivi `$help` per iniziare...")
+        } else {
+          embed.setDescription("**Canzoni in coda:**")
+          await Promise.all(coda.map(async (i) => {
+              const res = await yts(i);
+              let video = res["all"][0];
+              embed.addField(video.title, "[Guarda su YouTube.](" + video.url + ")");
+          }))
+        }
         message.channel.send(embed);
     }
     if(message.content == prefisso + "skip"){
@@ -180,6 +192,7 @@ async function Musica(message){
         }
     }
     if(message.content == prefisso + "isplaying"){
+        try {
         const res = await yts(CanzoneInRiproduzione);
         var video = res["all"][0]
         var embed = new Discord.MessageEmbed()
@@ -191,30 +204,43 @@ async function Musica(message){
         .addField("Pubblicato: ", video["ago"], true)
         .addField("Durata: ", video["duration"]["timestamp"], true)
         .setImage(video["image"])
+        .setColor("BLUE")
         message.channel.send(embed)
+        }
+        catch {
+            message.channel.send(":x: **Non sembrano esserci canzoni in riproduzione...**")
+        }
     }
     if(message.content == prefisso + "loop"){
         if(isLoop){
-            isLoop = false;
+            isLoop = "Ora il loop è **disattivato**.";
         } else {
-            isLoop = true;
+            isLoop = "Ora il loop è **attivato**.";
         }
-        message.channel.send("Loop: " + isLoop);
+        let embed = new Discord.MessageEmbed()
+        .setAuthor(message.author.tag, message.author.displayAvatarURL())
+        .setDescription(isLoop)
+        .setColor("BLUE")
+        message.channel.send(embed);
     }
     if(message.content == prefisso + "pause"){
         if(dispatcher.paused){
-            return message.channel.send("Canzone già in pausa");
+            return message.channel.send(":x: **La canzone è già in pausa.**");
         }
         dispatcher.pause();
-        message.channel.send("Canzone in pausa");
+        let embed = new Discord.MessageEmbed()
+        .setAuthor(message.author.tag, message.author.displayAvatarURL())
+        .setDescription("**Canzone in pausa!**")
+        .setColor("BLUE")
+        message.channel.send(embed);
     }
     if(message.content == prefisso + "resume"){
         if(!dispatcher.paused){
-            return message.channel.send("Canzone già in play");
+            return message.channel.send(":x: **La canzone è già in riproduzione.**");
         }
         var voiceChannel = message.member.voice.channel;
         if(voiceChannel == null){
-            message.channel.send("non sei in un canale vocale");
+            message.channel.send(":x: **Non sei in un canale vocale.**");
             return;
         }
         voiceChannel.join().then(connection =>{
@@ -233,13 +259,33 @@ async function Musica(message){
                 }
             });
         }).catch(err => console.log(err));
-        message.channel.send("Riproduco la canzone");
+        let embed2 = new Discord.MessageEmbed()
+        .setAuthor(message.author.tag, message.author.displayAvatarURL())
+        .setDescription("*Riproduco la canzone...**")
+        .setColor("BLUE")
+        message.channel.send(embed);
     }
     if(message.content == prefisso + "leave"){
         coda.length = 0;
         CanzoneInRiproduzione = null;
+        if(!message.guild.me.voice.channel) return message.channel.send(":x: **Non sono collegato in nessun canale vocale.**")
         const connection = await message.member.voice.channel;
         connection.leave();
-        message.channel.send("Esco dal canale vocale");
+        let embed = new Discord.MessageEmbed()
+        .setAuthor(message.author.tag, message.author.displayAvatarURL())
+        .setColor("BLUE")
+        .setDescription("**Esco dal canale vocale...**")
+        message.channel.send(embed);
+    }
+    if(message.content == prefisso + "join"){
+        coda.length = 0;
+        CanzoneInRiproduzione = null;
+        const connection = await message.member.voice.channel;
+        connection.join()
+        let embed = new Discord.MessageEmbed()
+        .setAuthor(message.author.tag, message.author.displayAvatarURL())
+        .setColor("BLUE")
+        .setDescription("**Sto entrando nel canale vocale...**")
+        message.channel.send(embed);
     }
 }
